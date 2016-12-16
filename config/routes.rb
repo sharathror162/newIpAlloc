287b1e8a32bde8d0d1db58285beacdc497cdfdfb
sharathchandrakannam <sharath.ror@gmail.com>
2016-12-15 23:26:33 -0800

    included table style, base root route and changed table span

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   match '/device/assign', to: 'devices#assign_ip_address', via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'devices#index'
 end
