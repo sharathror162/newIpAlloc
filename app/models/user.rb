d8ecc8d75ee514ecbd88b7c6d08dce313a2e7666
sharathchandrakannam <sharath.ror@gmail.com>
2016-12-21 18:17:17 -0800

    fixed email issue in signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
 
   validates :login, presence: true, uniqueness: true
-  validates :email, presence: true
-  validates :email, format: { with: /\A[a-zA-Z0-9]+@gmail.com\z/ }
+  validates :email, presence: true, format: { with: /\A\w+\.\w+@gmail.com\z/}
 
   has_secure_password
   has_many :devices, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220081624) do
+ActiveRecord::Schema.define(version: 20161220220046) do
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
@@ -29,20 +29,9 @@ ActiveRecord::Schema.define(version: 20161220081624) do
 
   create_table "devices", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.index ["email"], name: "index_devices_on_email"
-    t.index ["reset_password_token"], name: "index_devices_on_reset_password_token", unique: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "ip_addresses", force: :cascade do |t|
